f7301fea71233178841b3ae86fe718e265367109
GuiB-dv <152321046+GuiB-dv@users.noreply.github.com>
2024-07-08 14:35:27 -0300

    update and delete
    fixing views, who can access this information

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -6,14 +6,12 @@ const validateMessage = require('../config/messageValidatorConfig')
 
 exports.display_messages = asyncHandler(async(req, res, next) => {
     const allMessages = await Message.find().populate('author').sort({timestamp: -1}).exec()
-    const user = req.user;
     if(allMessages) {
         res.render ('index', {
             title: 'Welcome',
             msg: allMessages,
-            user: user
+            user: req.user
         })
-        console.log(allMessages)
     } else {
         console.log('No messages found')
         res.render('index', {title: 'Clubhouse'})
@@ -24,8 +22,7 @@ exports.display_messages = asyncHandler(async(req, res, next) => {
 exports.message_detail = asyncHandler(async(req, res, next) => {
     const message = await Message.findById(req.params.id)
         .populate("author")
-        .exec()
-    const user = req.user;    
+        .exec()    
     if (message === null) {
         const err = new Error("Message not found");
         err.status = 404;
@@ -34,7 +31,7 @@ exports.message_detail = asyncHandler(async(req, res, next) => {
     res.render("message_detail", {
         title: "Message",
         message: message,
-        user: user
+        user: req.user,
     })
 })
 
@@ -77,5 +74,10 @@ exports.delete_message_get = asyncHandler(async(req, res, next) => {
 })
 
 exports.delete_message_post = asyncHandler(async(req, res, next) => {
-    res.render('message_details', {title: 'Delete message NOT IMPLEMENTED'})
+    try {
+        await Message.findByIdAndDelete(req.body.messageid)
+        res.redirect('/')
+    } catch(err) {
+        return next(err)
+    }
 })
