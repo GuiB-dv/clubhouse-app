39d9ea4083fb91d8293a9b339712d53d455a206e
GuiB-dv <152321046+GuiB-dv@users.noreply.github.com>
2024-07-10 16:35:08 -0300

    cookie secure false

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true } 
+  cookie: { secure: false } 
   })); 
 app.use(passport.initialize());
 app.use(passport.session());
